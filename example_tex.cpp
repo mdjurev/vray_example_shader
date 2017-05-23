c6cb5324c9de9efc85da5563b9630236a9ec42ed
Mihail Djurev <mihail.djurev@chaosgroup.com>
2017-05-23 06:54:29 +0300

    Added another mode for the texture that uses UVs

--- example_tex.cpp
+++ example_tex.cpp
@@ -20,8 +20,12 @@ struct TexExample_Params: VRayParameterListDesc {
 	TexExample_Params(void) {
 		addParamTexture("texa", -1, "Texture input A", "displayName=(Texture A)");
 		addParamTexture("texb", -1, "Texture input B", "displayName=(Texture B)");
-		addParamInt("operation", 0, -1, "The mode of operation: 0 - add A and B; 1 - multiply A and B;", "displayName=(Mode), enum=(0:Add;1:Multiply;)");
+		addParamInt("operation", 0, -1, "The mode of operation: 0 - add A and B; 1 - multiply A and B; 2 - mix A and B using ratio; 3 - mix A and B using UVs",
+			"displayName=(Mode), enum=(0:Add;1:Multiply;2:Mix by ratio;3:Mix by UV)");
 		addParamTextureFloat("ratio", -1, "The ratio of mixing A/B. 0-A only, 1-B only.", "displayName=(Ratio), enable=(::operation=2)");
+		// Note: When implementing a texture with UVs, the texture class should extend MappedTexture, which provides it's own uvwgen parameter.
+		// Here, for the sake of simplicity, we create one ourselves.
+		addParamPlugin("uvwgen", EXT_UVWGEN, -1, "The UV generator. Used only when mode is 3.");
 	}
 };
 
@@ -33,9 +37,12 @@ struct TexExample: VRayPlugin, TextureInterface {
 		paramList->setParamCache("texb", &texb);
 		paramList->setParamCache("operation", &operation);
 		paramList->setParamCache("ratio", &ratio);
+		paramList->setParamCache("uvwgen", &uvwgen_plugin);
 		texa=texb=NULL;
 		ratio=NULL;
 		operation=0;
+		uvwgen_plugin=NULL;
+		uvwgen=NULL;
 	}
 
 	/// Destructor.
@@ -50,8 +57,15 @@ struct TexExample: VRayPlugin, TextureInterface {
 	PluginBase* getPlugin(void) VRAY_OVERRIDE { return this; }
 
 	//// From VRayPlugin
-	//void frameBegin(VRayRenderer *vray) VRAY_OVERRIDE {VRayPlugin::frameBegin(vray);};
-	//void frameEnd(VRayRenderer *vray) VRAY_OVERRIDE {VRayPlugin::frameEnd(vray);};
+	void frameBegin(VRayRenderer *vray) VRAY_OVERRIDE {
+		// This populates the parameter caches, so it's very important to call:
+		VRayPlugin::frameBegin(vray);
+		uvwgen = queryInterface<UVWGenInterface>(uvwgen_plugin, EXT_UVWGEN);
+	}
+	void frameEnd(VRayRenderer *vray) VRAY_OVERRIDE {
+		VRayPlugin::frameEnd(vray);
+		uvwgen = NULL;
+	}
 
 	// From TextureInterface	
 	/// Returns the value of the texture for the point in the given context.
@@ -66,6 +80,17 @@ struct TexExample: VRayPlugin, TextureInterface {
 			float r = ratio ? ratio->getTexFloat(rc) : 0.5f;
 			return a*r+(1.f-r)*b;
 		}
+		else if(operation=3) {
+			Vector uvw(0.f);
+			bool valid=false;
+			if(uvwgen) {				
+				uvw = uvwgen->getUVW(rc, valid);
+			}
+			if(!valid) {
+				uvw.makeZero();
+			}
+			return a*uvw.x+b*uvw.y;
+		}
 		AColor zero; zero.makeZero();
 		return zero;
 	}
@@ -79,7 +104,7 @@ struct TexExample: VRayPlugin, TextureInterface {
 		if(texb) texb->getTexColorBounds(bmin, bmax);
 		// Calculate the bounds for the result. The correct result here would mean that the 
 		// texture behaves predictably in displacement
-		if(operation==0 || operation==2) {
+		if(operation==0 || operation==2 || operation==3) {
 			fmax = amax+bmax;
 			fmin = VR::Min(amin, bmin);
 		}
@@ -99,6 +124,8 @@ private:
 	TextureInterface *texb;
 	int operation;
 	TextureFloatInterface *ratio;
+	PluginBase *uvwgen_plugin;
+	UVWGenInterface *uvwgen;
 };
 
 
